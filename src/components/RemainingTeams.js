d6e5e8c35bafdaa4557f026f68becf45f891fdc8
ismail <ismailmo4@gmail.com>
2022-01-29 17:49:18 +0000

    added invalid team styling

--- src/components/RemainingTeams.js
+++ src/components/RemainingTeams.js
@@ -1,10 +1,23 @@
+import styles from "./RemainingTeams.module.css";
+
 const RemainingTeams = (props) => {
+    const validTeamNames = props.validTeams.map((team) => team.name);
+
     return (
         <>
             <h2>Remaining Teams:</h2>
             <ul>
                 {props.remainingTeams.map((team) => (
-                    <li key={team.name}>{team.name}</li>
+                    <li
+                        className={
+                            validTeamNames.indexOf(team.name) >= 0
+                                ? ""
+                                : styles.invalid
+                        }
+                        key={team.name}
+                    >
+                        {team.name}
+                    </li>
                 ))}
             </ul>
         </>
